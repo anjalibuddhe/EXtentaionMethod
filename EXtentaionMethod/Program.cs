7264c0da16caa7ff6cc8e99c40e5f211e540c739
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-12 16:02:25 +0530

    Add project files.

---
+++ EXtentaionMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXtentaionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c  =new Calculation();
+           
+            c.Multiply(2, 3);
+            int res=c.Square(2);
+            Console.WriteLine(res);
+        }
+    }
+}
